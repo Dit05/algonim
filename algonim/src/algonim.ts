89979db651291da649ee74369518ffffabfb7988
kiki <mullerkrisztianbusiness@gmail.com>
2025-11-25 15:22:15 +0100

    Canvas clipping

--- algonim/src/algonim.ts
+++ algonim/src/algonim.ts
@@ -2,9 +2,16 @@ class Algonim extends HTMLElement {
 
   static observedAttributes = ["color", "shape"]
 
+  canvas: HTMLCanvasElement
+
+
   constructor() {
-    console.log('Algonim constructed :-)')
     super()
+    const shadow = this.attachShadow({ 'mode': 'closed' })
+    this.canvas = document.createElement('canvas')
+    this.canvas.width = 640
+    this.canvas.height = 480
+    shadow.appendChild(this.canvas)
   }
 
 
@@ -30,6 +37,14 @@ class Algonim extends HTMLElement {
 
   redraw() {
     console.log('Redraw called')
+
+    const CONTEXT_ID = '2d'
+    const ctx = this.canvas.getContext(CONTEXT_ID)
+    if(ctx === null) throw new ReferenceError(`canvas context '${CONTEXT_ID}' not supported or the canvas has already been set to a different mode`)
+    let drawer = new Drawer(ctx, new Region(0, 0, this.canvas.width, this.canvas.height))
+    drawer.fill('#ff00ff')
+    drawer = new Drawer(ctx, new Region(ctx.canvas.width / 4, ctx.canvas.height / 4, this.canvas.width / 2, this.canvas.height / 2))
+    drawer.fill('#ff0000')
   }
 
 }
@@ -61,10 +76,12 @@ class Region {
 
 }
 
+type CanvasStyle = string | CanvasGradient | CanvasPattern
 class Drawer {
 
   readonly context: CanvasRenderingContext2D
   readonly region: Region
+
 
   constructor(context: CanvasRenderingContext2D, region: Region) {
     this.context = context
@@ -72,11 +89,30 @@ class Drawer {
   }
 
 
-  public drawLine(startX: number, startY: number, endX: number, endY: number) {
+  doClipped(fn: () => void) {
+    this.context.save()
+    this.context.translate(this.region.x, this.region.y)
     this.context.beginPath()
-    this.context.moveTo(startX, startY)
-    this.context.lineTo(endX, endY)
-    this.context.stroke()
+    this.context.rect(0, 0, this.region.width, this.region.height)
+    fn()
+    this.context.restore()
+  }
+
+  public drawLine(startX: number, startY: number, endX: number, endY: number, strokeStyle: CanvasStyle) {
+    this.doClipped(() => {
+      this.context.beginPath()
+      this.context.moveTo(startX, startY)
+      this.context.lineTo(endX, endY)
+      this.context.strokeStyle = strokeStyle;
+      this.context.stroke()
+    })
+  }
+
+  public fill(fillStyle: CanvasStyle) {
+    this.doClipped(() => {
+      this.context.fillStyle = fillStyle;
+      this.context.fillRect(0, 0, this.region.width, this.region.height)
+    })
   }
 
 }
