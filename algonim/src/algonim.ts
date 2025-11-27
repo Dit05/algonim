6ecc3f803ed4c04aa15a67f7ef08c1b358f0e8c7
Dit05 <mullerkrisztianbusiness@gmail.com>
2025-11-27 22:45:27 +0100

    Split pane

--- algonim/src/algonim.ts
+++ algonim/src/algonim.ts
@@ -1,3 +1,5 @@
+
+
 class Algonim extends HTMLElement {
 
   static observedAttributes = ["color", "shape"]
@@ -7,6 +9,8 @@ class Algonim extends HTMLElement {
 
   constructor() {
     super()
+
+    // Create shadow canvas
     const shadow = this.attachShadow({ 'mode': 'closed' })
     this.canvas = document.createElement('canvas')
     this.canvas.width = 640
@@ -41,12 +45,70 @@ class Algonim extends HTMLElement {
     const CONTEXT_ID = '2d'
     const ctx = this.canvas.getContext(CONTEXT_ID)
     if(ctx === null) throw new ReferenceError(`canvas context '${CONTEXT_ID}' not supported or the canvas has already been set to a different mode`)
-    let drawer = new Drawer(ctx, new Region(0, 0, this.canvas.width, this.canvas.height))
-    drawer.fill('#ff00ff')
+
+    const drawer = new Drawer(ctx, new Region(0, 0, this.canvas.width, this.canvas.height))
+    drawer.fill("white");
+
+    const split = new SplitPane()
+
+    {
+      const pane = new ModelPane()
+      const code = new CodeModel()
+      pane.model = code
+      split.second = code
+    }
+
+    split.draw(drawer)
+
+    /*drawer.fill('#ff00ff')
     drawer = new Drawer(ctx, new Region(ctx.canvas.width / 4, ctx.canvas.height / 4, this.canvas.width / 2, this.canvas.height / 2))
     drawer.fill('#ff0000')
+    drawer.drawLine(0, 0, 800, 800, "black") // SEE: this line is clipped to the small area*/
   }
 
+}
+
+
+abstract class Pane {
+  public abstract draw(drawer: Drawer): void
+}
+
+class ModelPane implements Pane {
+  model: Model | null = null
+
+  public draw(drawer: Drawer) {
+    if(this.model !== null) {
+      this.model.draw(drawer)
+    }
+  }
+}
+
+enum Axis {
+  Horizontal,
+  Vertical
+}
+
+class SplitPane implements Pane {
+  axis: Axis = Axis.Horizontal
+  anchor: number = 0.5
+
+  first: Pane | null = null
+  second: Pane | null = null
+
+
+  public draw(drawer: Drawer) {
+    // TODO axis
+    if(this.first !== null) {
+      const firstDrawer = drawer.subregion(drawer.getLocalRegion().hsplit(0.0, this.anchor))
+      this.first.draw(firstDrawer)
+    }
+    if(this.second !== null) {
+      const secondDrawer = drawer.subregion(drawer.getLocalRegion().hsplit(this.anchor, 1.0))
+      this.second.draw(secondDrawer)
+    }
+    const x = drawer.getLocalRegion().width * this.anchor
+    drawer.drawLine(x, 0, x, drawer.getLocalRegion().height, 'black')
+  }
 }
 
 
@@ -74,6 +136,55 @@ class Region {
     this.height = height
   }
 
+
+  public static fromStartEnd(startX: number, startY: number, endX: number, endY: number): Region {
+    return new Region(
+      startX,
+      startY,
+      Math.max(startX, endX) - startX,
+      Math.max(startY, endY) - startY
+    );
+  }
+
+  public subregion(sub: Region): Region {
+    return sub.offset(this.x, this.y).clippedBy(this)
+  }
+
+  public hsplit(startFac: number, endFac: number): Region {
+    endFac = Math.min(endFac, 1)
+    startFac = Math.min(Math.max(startFac, 0), endFac)
+    return Region.fromStartEnd(
+      this.x + startFac * this.width,
+      this.y,
+      this.x + endFac * this.width,
+      this.y + this.height
+    )
+  }
+
+  public vsplit(startFac: number, endFac: number): Region {
+    endFac = Math.min(endFac, 1)
+    startFac = Math.min(Math.max(startFac, 0), endFac)
+    return Region.fromStartEnd(
+      this.x,
+      this.y + startFac * this.height,
+      this.x + this.width,
+      this.y + endFac * this.height
+    )
+  }
+
+
+  public offset(x: number, y: number): Region {
+    return new Region(this.x + x, this.y + y, this.width, this.height)
+  }
+
+  public clippedBy(clip: Region): Region {
+    const startX = Math.max(this.x, clip.x)
+    const startY = Math.max(this.y, clip.y)
+    const endX = Math.min(this.x + this.width, clip.x + clip.width)
+    const endY = Math.min(this.y + this.height, clip.y + clip.height)
+    return Region.fromStartEnd(startX, startY, endX, endY)
+  }
+
 }
 
 type CanvasStyle = string | CanvasGradient | CanvasPattern
@@ -86,6 +197,16 @@ class Drawer {
   constructor(context: CanvasRenderingContext2D, region: Region) {
     this.context = context
     this.region = region
+    this.context.strokeStyle = "black"
+  }
+
+
+  public subregion(region: Region): Drawer {
+    return new Drawer(this.context, this.region.subregion(region))
+  }
+
+  public getLocalRegion(): Region {
+    return new Region(0, 0, this.region.width, this.region.height)
   }
 
 
@@ -94,6 +215,7 @@ class Drawer {
     this.context.translate(this.region.x, this.region.y)
     this.context.beginPath()
     this.context.rect(0, 0, this.region.width, this.region.height)
+    this.context.clip()
     fn()
     this.context.restore()
   }
@@ -108,6 +230,14 @@ class Drawer {
     })
   }
 
+  public drawText(text: string, x: number, y: number, fillStyle: CanvasStyle) {
+    this.doClipped(() => {
+      this.context.font = "48px sans" // TODO better
+      this.context.fillStyle = fillStyle;
+      this.context.fillText(text, x, y)
+    })
+  }
+
   public fill(fillStyle: CanvasStyle) {
     this.doClipped(() => {
       this.context.fillStyle = fillStyle;
@@ -119,11 +249,16 @@ class Drawer {
 
 
 abstract class Model {
+  public abstract draw(drawer: Drawer): void
 }
 
 class CodeModel extends Model {
 
   lines: string[] = []
+
+  public draw(drawer: Drawer) {
+    drawer.drawText("foobar", 50, 90, "black")
+  }
 
 }
 
