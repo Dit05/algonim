6ac14e3a87582c6fede9f612a60a4a3eed064226
Dit05 <mullerkrisztianbusiness@gmail.com>
2026-02-21 03:09:23 +0100

    CodeModel line numbering and styling properties

--- algonim/src/models/Code.ts
+++ algonim/src/models/Code.ts
@@ -1,7 +1,7 @@
 import { Model } from './Model'
 import { Drawer } from '@/gfx/Drawer'
 import { Point, Size, SizeUtil } from '@/gfx/Primitives'
-import { TextAlign, FontStyle } from '@/gfx/Styles'
+import { TextAlign, FontStyle, LineStyle, ArrowStyle } from '@/gfx/Styles'
 import { TextWrapper, TextAtom, ClipResult, Text, SpaceAtom } from '@/gfx/TextWrapper'
 import * as CONFIG from '@/config'
 
@@ -11,6 +11,7 @@ class Line {
   signs: Sign[] = []
 }
 
+// TODO put a box around it or something
 export class Sign implements TextAtom {
 
   static readonly ALIGN: TextAlign = { align: 'left', baseline: 'top' }
@@ -60,20 +61,54 @@ export type SignStyle = {
 export class Code extends Model {
 
   lines: Line[] = []
+  /** Lines that should have an arrow before them. Can be a `number`, a `function` mapping line numbers to `boolean`s, or `null` to not have any arrows. */
   public arrowLines: number | ((line: number) => boolean) | null = null
 
   // Styling
+  /** Position where drawing begins. */
+  public origin: Point = Point(12, 12)
+
+  /** Style applied to most text. */
+  public textStyle: Partial<FontStyle> = {}
+  /** Squash/stretch the distance between lines. */
   public textHeightFactor: number = 0.8
 
-  // TODO further customizability
   /** Distance between the end of the line and the start of `Sign`s. */
   public signSeparation: number = 12
   /** Distance between individual `Sign`s. */
   public signSpacing: number = 8
-
-  /** Style applied to newly created `Sign`s. */
+  /** Style applied to newly created `Sign`s. The font style is not inherited from `textStyle`. @see textStyle */
   public defaultSignStyle: SignStyle = {
     fontStyle: { fill: 'blue' }
+  }
+
+  /** Number of the first visible line. */
+  public numberingStart: number = 1
+  /**
+  * Overrides the main text style for line numbers. Setting this to null will cause lines to not be numbered. Note that the number separator needs to be disabled separately.
+  * @see textStyle
+  * @see numberSeparatorStyle
+  */
+  public numberingStyleOverride: Partial<FontStyle> | null = { fill: 'gray' }
+  /** Style applied to the line separating a line's numbering from its content. */
+  public numberSeparatorStyle: Partial<LineStyle> | null = { stroke: 'gray' }
+  /** Space before and after the number separator. @see numberSeparatorStyle */
+  public readonly numberSeparatorMargins = {
+    'before': 4,
+    'after': 12
+  }
+
+  /** Space between the tip of an arrow and the start of the line content it's pointing at. */
+  public arrowDistance: number = 6
+  /** Horizontal length of an arrow's main line. */
+  public arrowLength: number = 24
+  /** Style applied to arrows. */
+  public readonly arrowStyle: {
+    line: Partial<LineStyle>,
+    head: ArrowStyle
+  } = {
+    line: { stroke: 'red', lineWidth: 2 },
+    head: { length: 10, angleDegrees: 30 }
   }
   //
 
@@ -91,8 +126,9 @@ export class Code extends Model {
     return new Sign(line, this.defaultSignStyle)
   }
 
-  static measureIndent(text: string): number {
+  static measureAndRemoveIndent(text: string): { indent: number, deindented: string } {
     let indent = 0
+    let removeCount = 0
     for(let i = 0; i < text.length; i++) {
       let done = false
       switch(text[i]) {
@@ -101,8 +137,12 @@ export class Code extends Model {
         default: done = true; break
       }
       if(done) break
+      else removeCount += 1
     }
-    return indent
+    return {
+      indent: indent,
+      deindented: text.substring(removeCount)
+    }
   }
 
   public draw(drawer: Drawer) {
@@ -122,22 +162,45 @@ export class Code extends Model {
         break
     }
 
-    const style: Partial<FontStyle> = { fill: 'black' }
-    const em: number = drawer.measureText('m', {}, style).width
+    const em: number = drawer.measureText('m', {}, this.textStyle).width
 
     const wrapper = new TextWrapper(drawer)
     wrapper.textHeightFactor = this.textHeightFactor
 
-    // TODO numbered lines
+    let y = this.origin.y
+    const beginX = this.origin.x
 
-    let y = 12
-    const gutterWidth = 24
+    const effectiveNumberingStyle: Partial<FontStyle> | null = this.numberingStyleOverride !== null ? { ...this.textStyle, ...this.numberingStyleOverride } : null
+    let numbersWidth = 0
+    if(effectiveNumberingStyle !== null) {
+      for(let i = 0; i < this.lines.length; i++) {
+        numbersWidth = Math.max(numbersWidth, drawer.measureText(String(this.numberingStart + i), {}, effectiveNumberingStyle).width)
+      }
+    }
 
     for(let i = 0; i < this.lines.length; i++) {
       const line = this.lines[i]
-      const indentSize = Code.measureIndent(line.text) * em
+      let lineContent = line.text
 
-      const text: Text = [line.text]
+      let x = beginX
+
+      if(effectiveNumberingStyle !== null) {
+        drawer.drawText(String(this.numberingStart + i), Point(x, y), { align: 'left', baseline: 'top' }, effectiveNumberingStyle)
+        x += numbersWidth
+      }
+      let numberSeparatorX: number | null = null
+      if(this.numberSeparatorStyle !== null) {
+        x += this.numberSeparatorMargins.before
+        numberSeparatorX = x
+        x += this.numberSeparatorMargins.after
+      }
+
+      const indentInfo = Code.measureAndRemoveIndent(lineContent)
+      lineContent = indentInfo.deindented
+      x += indentInfo.indent * em
+      const arrowX = x
+
+      const text: Text = [lineContent]
       if(line.signs.length > 0) {
         text.push(new SpaceAtom(Size(this.signSeparation, 0)))
         const spacer = new SpaceAtom(Size(this.signSpacing, 0))
@@ -147,14 +210,17 @@ export class Code extends Model {
         }
       }
 
-      const x = gutterWidth + indentSize
-      const drawResult = wrapper.drawText(text, Point(x, y), drawer.getSize().width - x, style)
+      const drawResult = wrapper.drawText(text, Point(x, y), drawer.getSize().width - x, this.textStyle)
+
+      if(this.numberSeparatorStyle !== null && numberSeparatorX !== null) {
+        drawer.drawLine(Point(numberSeparatorX, y), Point(numberSeparatorX, y + drawResult.size.height - 1), this.numberSeparatorStyle)
+      }
 
       if(isArrowLine(i)) {
-        const start = Point(gutterWidth + indentSize - 12, y + drawResult.size.height / 2)
-        const end = Point(gutterWidth + indentSize - 2, y + drawResult.size.height / 2)
-        drawer.drawLine(start, end, { stroke: 'red' })
-        drawer.drawArrowhead(start, end, { stroke: 'red' }, { length: 4 })
+        const start = Point(arrowX - this.arrowDistance - this.arrowLength, y + drawResult.size.height / 2)
+        const end = Point(arrowX - this.arrowDistance, y + drawResult.size.height / 2)
+        drawer.drawLine(start, end, this.arrowStyle.line)
+        drawer.drawArrowhead(start, end, this.arrowStyle.line, this.arrowStyle.head)
       }
 
       y += drawResult.size.height
