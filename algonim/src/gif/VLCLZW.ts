89ed2e14bf94a8bda8628ebc07c153f5b53780d2
Dit05 <mullerkrisztianbusiness@gmail.com>
2026-03-16 13:15:15 +0100

    Make LZW yielding eager again

--- algonim/src/gif/VLCLZW.ts
+++ algonim/src/gif/VLCLZW.ts
@@ -12,33 +12,24 @@ interface ArrayLikeButWritable<T> extends ArrayLike<T> {
 class CompactBinaryTree<TElem, TArray extends ArrayLikeButWritable<TElem>> {
 
   public readonly elements: TArray
+  public readonly bitWidth: number
+
   private readonly leftPtrs: Int32Array
   private readonly rightPtrs: Int32Array
   private count: number = 0
+  public getCount(): number { return this.count }
 
 
-  public constructor(elements: TArray) {
+  public constructor(elements: TArray, bitWidth: number) {
     this.elements = elements
+    this.bitWidth = bitWidth
+
     this.leftPtrs = new Int32Array(elements.length)
     this.rightPtrs = new Int32Array(elements.length)
     this.leftPtrs.fill(-1)
     this.rightPtrs.fill(-1)
   }
 
-
-  public step(index: number, right: boolean): number {
-    return (right ? this.rightPtrs : this.leftPtrs)[index]
-  }
-
-  /*public append(index: number, right: boolean): number {
-    if(this.count >= this.elements.length) throw new RangeError("Tree full.")
-
-    const array = (right ? this.rightPtrs : this.leftPtrs)
-    if(array[index] !== -1) throw new RangeError("Node already present.")
-
-    this.count += 1
-    return (array[index] = this.count)
-  }*/
 
   private insert(index: number, bits: number, size: number): number {
     while(size --> 0) {
@@ -117,7 +108,7 @@ class CompactBinaryTree<TElem, TArray extends ArrayLikeButWritable<TElem>> {
 */
 export function* compress(data: Iterable<number>, initialCodeSize: number): Generator<number> {
   if(initialCodeSize < 2) throw new RangeError("Initial code size must be at least 2.")
-  // TODO fail if an element lays outside the initial code size
+  if(initialCodeSize > 8) throw new RangeError("Initial code size must be at most 8.")
 
   const CODE_CLEAR = (1 << initialCodeSize)
   const CODE_END = CODE_CLEAR + 1
@@ -130,8 +121,8 @@ export function* compress(data: Iterable<number>, initialCodeSize: number): Gene
   const output: number[] = []
   const bitVector = new BitVector((byte) => output.push(byte))
 
-  const tree = new CompactBinaryTree<number, Int32Array>(new Int32Array((CODE_MAX + 1) * initialCodeSize)) // TODO + 2?
-  //tree.trim(0, -1) // Fill with -1 so that getTextRepresentation.
+  const tree = new CompactBinaryTree<number, Int32Array>(new Int32Array((CODE_MAX + 1) * initialCodeSize), initialCodeSize) // TODO + 2?
+  tree.trim(0, -1)
   for(let i = 0; i < (1 << initialCodeSize); i++) {
     tree.elements[-(tree.traverseOrInsert(0, i, initialCodeSize) + 1)] = i
   }
@@ -139,58 +130,45 @@ export function* compress(data: Iterable<number>, initialCodeSize: number): Gene
   bitVector.add(CODE_CLEAR, codeSize)
 
   let match = 0
-
-  //const codestream = []
-
   for(const sym of data) {
+    if(sym < 0 || sym >= (1 << initialCodeSize)) throw new RangeError(`'${sym}' falls outside the representable range of [${0}..${(1 << initialCodeSize) - 1}].`)
+
     let nextMatch = tree.traverseOrInsert(match, sym, initialCodeSize)
     if(nextMatch >= 0) {
       // Found
       match = nextMatch
-      //console.log(`matched ${sym}`)
     } else {
       // Not found
-      //console.log(`not-matched ${sym}, adding ${nextCode}`)
       tree.elements[-(nextMatch + 1)] = nextCode++
-      //console.log(tree.getTextRepresentation(x => x >= 0, initialCodeSize))
 
       const code = tree.elements[match]
       bitVector.add(code, codeSize)
-      //console.log(`emitting ${code} with size ${codeSize}`)
-      //codestream.push(code)
 
       if(nextCode > CODE_MAX) {
         bitVector.add(CODE_CLEAR, codeSize)
         codeSize = 1
         tree.trim(1 << initialCodeSize, -1)
-        //console.log(`code size resetting to ${codeSize}`)
       } else if(nextCode > (1 << codeSize)) {
         codeSize += 1
-        //console.log(`code size increasing to ${codeSize}`)
       }
 
       match = tree.traverseOrInsert(0, sym, initialCodeSize)
       if(match < 0) throw new Error("This should never happen.")
     }
+
+    for(let i = 0; i < output.length; i++) {
+      yield output[i] 
+    }
+    output.length = 0
   }
 
   bitVector.add(tree.elements[match], codeSize)
   bitVector.add(CODE_END, codeSize)
   bitVector.flush()
 
-  //console.log(codestream)
-
   for(let i = 0; i < output.length; i++) {
     yield output[i] 
   }
+  output.length = 0
 
-
-  /*bitVector.add(CODE_END, codeSize)
-
-  bitVector.flush()
-  if(output.length > 0) {
-    for(let byte of output) {
-      yield byte
-    }
-  }*/
 }
